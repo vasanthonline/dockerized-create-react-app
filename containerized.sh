cd6929a7d3c3381954383f2a184ae94925aed80d
Arvind Nedumaran <arvind@mailage.co>
2021-06-07 07:10:12 +0530

    Streamlined containerized.sh

--- containerized.sh
+++ containerized.sh
@@ -2,21 +2,10 @@
 set -eo pipefail
 BUILD_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-RUN_COMMAND=""
-
-case "$1" in
-    --no-build)
-        echo "Skip building a docker image. Using existing image."
-        COMPOSE_ARGS="--no-build"
-        RUN_COMMAND=${@:2}
-        ;;
-    *)
-        RUN_COMMAND=${@}
-        ;;
-esac
-
-
+RUN_COMMAND=${@}
 
 if [[ -n $RUN_COMMAND ]]; then
     docker-compose run client ${RUN_COMMAND}
+else
+    docker-compose build client
 fi
